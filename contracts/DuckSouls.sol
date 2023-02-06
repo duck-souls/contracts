84809989ee6cf473725a3a5503c71d9c09733a90
marcoruggeri <4high20@gmail.com>
2023-02-06 07:41:50 +0000

    add functionalities

--- contracts/DuckSouls.sol
+++ contracts/DuckSouls.sol
@@ -5,8 +5,62 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Enumer
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract DuckSouls is ERC721EnumerableUpgradeable, OwnableUpgradeable {
-    function initialize() public initializer {
+    struct Duck {
+        uint256 value;
+        uint256 lastInteraction;
+        uint8 hp;
+        bool alive;
+    }
+
+    mapping(uint256 => Duck) public ducks;
+
+    function initialize() external initializer {
         __ERC721_init("Duck Souls", "DUCKS");
         __Ownable_init();
+    }
+
+    function mint() external payable onlyOwner {
+        require(msg.value == 0.1 ether, "wrong value");
+        uint256 tokenId = totalSupply() + 1;
+        _safeMint(msg.sender, tokenId);
+        ducks[tokenId] = Duck(0.1 ether, block.timestamp, 3, true);
+    }
+
+    function shoot(uint256 _duckId) external {
+        require(
+            ducks[_duckId].lastInteraction < block.timestamp + 12 hours,
+            "not ready"
+        );
+        ducks[_duckId].hp -= 1;
+        ducks[_duckId].lastInteraction = block.timestamp;
+    }
+
+    function action(uint256 _duckId) external payable {
+        require(
+            ducks[_duckId].lastInteraction < block.timestamp + 8 hours,
+            "not ready"
+        );
+        require(msg.value == 0.1 ether, "wrong value");
+        require(msg.sender == ownerOf(_duckId), "not owner");
+        ducks[_duckId].value += 0.1 ether;
+        ducks[_duckId].lastInteraction = block.timestamp;
+    }
+
+    function kill(uint256 _duckId) external {
+        require(ducks[_duckId].hp == 0, "not dead");
+        ducks[_duckId].value = ducks[_duckId].value / 2;
+        payable(msg.sender).transfer(ducks[_duckId].value / 10);
+    }
+
+    function withdraw(uint256 _duckId) external {
+        require(ducks[_duckId].hp != 0, "dead");
+        require(msg.sender == ownerOf(_duckId), "not owner");
+        transferFrom(
+            msg.sender,
+            0x000000000000000000000000000000000000dEaD,
+            _duckId
+        );
+        (bool sent, ) = msg.sender.call{value: ducks[_duckId].value}("");
+        require(sent, "Failed to send Ether");
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
+  // hardhat: {
+  //   forking: {
+  //     url: process.env.MUMBAI_URL || "",
+  //   },
+  //   // blockGasLimit: 20000000,
+  //   // gas: 12000000,
+  //   // allowUnlimitedContractSize: true,
+  // },
+  networks: {
+    hyperspace: {
+      chainId: 3141,
+      url: "https://api.hyperspace.node.glif.io/rpc/v1",
+      //@ts-ignore
+      accounts: [process.env.PK],
+    },
+  },
 };
 
 export default config;
